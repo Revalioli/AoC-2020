3e1b66ac0933945c1f4dd4fb5a559ffc52e1ccc0
Revalioli <emmanuel.montmasson@gmail.com>
2020-12-05 14:51:43 +0100

    Just discovered tr method for String

--- Day5/day5_2.rb
+++ Day5/day5_2.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
+
+# seats = IO.readlines("input", chomp: true).map{|seat|
+#     seat[0,7].split("").map(&{"F" => "0", "B" => "1"}.to_proc).join.to_i(2) * 8 + seat[-3,3].split("").map(&{"L" => "0", "R" => "1"}.to_proc).join.to_i(2)
+# }.sort
+
 seats = IO.readlines("input", chomp: true).map{|seat|
-    seat[0,7].split("").map(&{"F" => "0", "B" => "1"}.to_proc).join.to_i(2) * 8 + seat[-3,3].split("").map(&{"L" => "0", "R" => "1"}.to_proc).join.to_i(2)
+    seat[0,7].tr("FB","01").to_i(2) * 8 + seat[-3,3].tr("LR","01").to_i(2)
 }.sort
 
 puts (seats.min..seats.max).to_a - seats
