66f9e1304cccf30fe2b239ba6a659b6b66003a6f
Revalioli <elinor.montmasson@gmail.com>
2020-12-05 14:39:47 +0100

    Day5 part 1 & 2 in Ruby

---
+++ Day5/day5_1.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+puts IO.readlines("input", chomp: true).map{|seat|
+    seat[0,7].split("").map(&{"F" => "0", "B" => "1"}.to_proc).join.to_i(2) * 8 + seat[-3,3].split("").map(&{"L" => "0", "R" => "1"}.to_proc).join.to_i(2)
+}.max
