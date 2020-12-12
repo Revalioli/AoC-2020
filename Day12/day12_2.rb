c4b345952a359bf2bc4928ec30cce698bcb9cf78
Revalioli <emmanuel.montmasson@gmail.com>
2020-12-12 22:41:30 +0100

    Day12 part 1 & 2 in Ruby

---
+++ Day12/day12_2.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+waypoint = [10,1]
+x, y = 0, 0
+
+IO.readlines("input", chomp: true).each{ |instruct|
+    op = instruct[0]
+    val = instruct[1..].to_i
+
+    case op
+    when "E"
+        waypoint[0] += val
+    when "N"
+        waypoint[1] += val
+    when "W"
+        waypoint[0] -= val
+    when "S"
+        waypoint[1] -= val
+    when "L"
+        1.upto(val/90){ |i|
+            old_x, old_y = *waypoint
+            waypoint[1] = old_x
+            waypoint[0] = -old_y
+        }
+    when "R"
+        1.upto(val/90){|i|
+            old_x, old_y = *waypoint
+            waypoint[0] = old_y
+            waypoint[1] = -old_x
+        }
+    when "F"
+        x += waypoint[0] * val
+        y += waypoint[1] * val
+    end
+
+    puts "x : " + x.to_s + " y : " + y.to_s
+}
+
+puts x
+puts y
+puts x.abs + y.abs
